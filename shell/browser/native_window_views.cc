36ff6c3579b5ced7babac8188c5e40fd767f8546
Lin Cheng <lin_cheng@dell.com>
2023-04-14 08:14:38 +0000

    src: add supported skiptaskbar linux impl
    Co-authored-by: Lin Cheng <lin_cheng@dell.com>

--- shell/browser/native_window_views.cc
+++ shell/browser/native_window_views.cc
@@ -310,6 +310,11 @@ NativeWindowViews::NativeWindowViews(const gin_helper::Dictionary& options,
     // to manually set the _NET_WM_STATE.
     std::vector<x11::Atom> state_atom_list;
 
+    bool skip_taskbar = false;
+    if (options.Get(options::kSkipTaskbar, &skip_taskbar) && skip_taskbar) {
+      state_atom_list.push_back(x11::GetAtom("_NET_WM_STATE_SKIP_TASKBAR"));
+    }
+
     if (top) {
       state_atom_list.push_back(x11::GetAtom("_NET_WM_STATE_ABOVE"));
     }
@@ -1046,6 +1051,10 @@ void NativeWindowViews::SetSkipTaskbar(bool skip) {
     taskbar->AddTab(GetAcceleratedWidget());
     taskbar_host_.RestoreThumbarButtons(GetAcceleratedWidget());
   }
+#elif defined(USE_OZONE_PLATFORM_X11)
+  if (IsX11())
+    SetWMSpecState(static_cast<x11::Window>(GetAcceleratedWidget()), skip,
+                   x11::GetAtom("_NET_WM_STATE_SKIP_TASKBAR"));
 #endif
 }
 

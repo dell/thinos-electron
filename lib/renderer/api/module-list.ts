5ab95721d795bb2643e6c6cd48d54e013c0e0318
Lin Cheng <lin_cheng@dell.com>
2023-10-12 09:18:00 +0000

    src: electron add ipcDbus API
    Co-authored-by: Lin Cheng <lin_cheng@dell.com>

--- lib/renderer/api/module-list.ts
+++ lib/renderer/api/module-list.ts
@@ -3,6 +3,7 @@ export const rendererModuleList: ElectronInternal.ModuleEntry[] = [
   { name: 'clipboard', loader: () => require('./clipboard') },
   { name: 'contextBridge', loader: () => require('./context-bridge') },
   { name: 'crashReporter', loader: () => require('./crash-reporter') },
+  { name: 'ipcDbus', loader: () => require('./ipc-dbus') },
   { name: 'ipcRenderer', loader: () => require('./ipc-renderer') },
   { name: 'webFrame', loader: () => require('./web-frame') },
   { name: 'webUtils', loader: () => require('./web-utils') }
